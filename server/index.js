c9ba7524a39d6333f8d77d24a07388debdf0183b
yashHindgreco <103257851+yashjangir3233@users.noreply.github.com>
2024-10-28 15:51:31 +0530

    please

--- server/index.js
+++ server/index.js
@@ -1,32 +1,67 @@
+// import express from 'express'
+// import cors from 'cors'
+// import bodyParser from 'body-parser'
+// import mongoose from 'mongoose'
+// import userRouter from './routes/user.js'
+
+
+// const URL =`mongodb+srv://maajack676:mongodbkapassword@cluster0.5olym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// const mongoConnection = async () => {
+//     try{
+//         await mongoose.connect(URL)
+//         console.log('database connected successfully')
+//     }catch(e){
+//         console.log(e.message)
+//     }
+// }
+// mongoConnection();
+// const app = express();
+
+// app.use(cors());
+// app.use(bodyParser.urlencoded({extended:true}))
+// app.use(bodyParser.json({extended:true}));
+// app.get('/',(req,res)=>{
+//     res.json('hello');
+// })
+// app.use(userRouter)
+
+// app.use('/fileview',express.static('./upload/images'));
+
+// app.listen(4000,() => {
+//     console.log('server running on port 4000')
+// })
+
+
 import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import userRouter from './routes/user.js'
 
+const URL = `mongodb+srv://maajack676:mongodbkapassword@cluster0.5olym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-const URL =`mongodb+srv://maajack676:mongodbkapassword@cluster0.5olym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-const mongoConnection = async () => {
-    try{
-        await mongoose.connect(URL)
-        console.log('database connected successfully')
-    }catch(e){
-        console.log(e.message)
-    }
-}
-mongoConnection();
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json({extended:true}));
-app.get('/',(req,res)=>{
-    res.json('hello');
+
+app.get('/', async (req, res) => {
+    // Connect to MongoDB for each request
+    try {
+        await mongoose.connect(URL)
+        res.json('hello');
+    } catch (e) {
+        res.status(500).json({ error: 'Database connection failed' });
+    } finally {
+        // Close the connection after the request is handled
+        await mongoose.connection.close();
+    }
 })
+
 app.use(userRouter)
 
-app.use('/fileview',express.static('./upload/images'));
+app.use('/fileview', express.static('./upload/images'));
 
-app.listen(4000,() => {
-    console.log('server running on port 4000')
-})
+// Export the Express app
+export default app;
