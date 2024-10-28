6609021975cdf007c6221d2d0ec51b2323092734
yashHindgreco <103257851+yashjangir3233@users.noreply.github.com>
2024-10-28 16:13:29 +0530

    please run

--- server/index.js
+++ server/index.js
@@ -1,32 +1,49 @@
-import express from 'express'
-import cors from 'cors'
-import bodyParser from 'body-parser'
-import mongoose from 'mongoose'
-import userRouter from './routes/user.js'
+// import express from 'express'
+// import cors from 'cors'
+// import bodyParser from 'body-parser'
+// import mongoose from 'mongoose'
+// import userRouter from './routes/user.js'
 
 
-// const URL =`mongodb+srv://maajack676:mongodbkapassword@cluster0.5olym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-// const mongoConnection = async () => {
-//     try{
-//         await mongoose.connect(URL)
-//         console.log('database connected successfully')
-//     }catch(e){
-//         console.log(e.message)
-//     }
-// }
-// mongoConnection();
-const app = express();
+// // const URL =`mongodb+srv://maajack676:mongodbkapassword@cluster0.5olym.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// // const mongoConnection = async () => {
+// //     try{
+// //         await mongoose.connect(URL)
+// //         console.log('database connected successfully')
+// //     }catch(e){
+// //         console.log(e.message)
+// //     }
+// // }
+// // mongoConnection();
+// const app = express();
 
-app.use(cors());
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json({extended:true}));
-app.get('/',(req,res)=>{
-    res.json('hello');
+// app.use(cors());
+// app.use(bodyParser.urlencoded({extended:true}))
+// app.use(bodyParser.json({extended:true}));
+// app.get('/',(req,res)=>{
+//     res.json('hello');
+// })
+// app.use(userRouter)
+
+// app.use('/fileview',express.static('./upload/images'));
+
+// app.listen(4000,() => {
+//     console.log('server running on port 4000')
+// })
+
+const express = require('express');
+
+const app = express()
+const PORT = 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
 })
-app.use(userRouter)
 
-app.use('/fileview',express.static('./upload/images'));
+app.get('/about', (req, res) => {
+  res.send('About route 🎉 ')
+})
 
-app.listen(4000,() => {
-    console.log('server running on port 4000')
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on port ${PORT}`);
 })
