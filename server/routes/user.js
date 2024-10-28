7d0afbb6909f77793e24c8950e35b9d94650022d
yashHindgreco <103257851+yashjangir3233@users.noreply.github.com>
2024-10-28 16:27:54 +0530

    lets try once again

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,18 +1,18 @@
 import express from 'express'
-import multer from 'multer';
-import path from 'path'
+// import multer from 'multer';
+// import path from 'path'
 import user from '../Schema/user.js';
 
 
 const userRouter = express.Router();
-var storage = multer.diskStorage({
-    destination:'./upload/images',
-    filename:(req,file,cb) => {
-        return cb(null,`${file.originalname.split('.')[0]}_${Date.now()}${path.extname(file.originalname)}`,);
-    }
-})
+// var storage = multer.diskStorage({
+//     destination:'./upload/images',
+//     filename:(req,file,cb) => {
+//         return cb(null,`${file.originalname.split('.')[0]}_${Date.now()}${path.extname(file.originalname)}`,);
+//     }
+// })
 
-var upload = multer({storage:storage});
+// var upload = multer({storage:storage});
 
 
 
@@ -28,15 +28,15 @@ userRouter.post('/register',async (req,res) => {
     }
 })
 
-userRouter.post('/upload',upload.single('file'),async(req,res) => {
-    try{
-        const file = req.file
-        res.status(200).json({msg:'file uploaded successfully',fileName:file.filename})
-    }catch(e){
-        console.log(e.message)
-        res.status(500).json({err:e.message,msg:'internal server error for file upload'})
-    }
-})
+// userRouter.post('/upload',upload.single('file'),async(req,res) => {
+//     try{
+//         const file = req.file
+//         res.status(200).json({msg:'file uploaded successfully',fileName:file.filename})
+//     }catch(e){
+//         console.log(e.message)
+//         res.status(500).json({err:e.message,msg:'internal server error for file upload'})
+//     }
+// })
 
 export default userRouter
 
